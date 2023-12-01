ed8f7a84f9cd159b80e0b337e7619077df13ebcb
Phạm Khang <ppkhang285@gmail.com>
2023-12-01 13:42:15 +0700

    Update NhanGiaoDichIn

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./components/homepage/homepage.component";
-import NhanGiaoDichIn from "./components/nhan-giao-dich-in-nvia/nhangiaodichin_nvia.component";
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Homepage />} /> */}
-        <Route path="/" element={<NhanGiaoDichIn />} />
+        <Route path="/" element={<Homepage />} />
       </Routes>
     </div>
   );
