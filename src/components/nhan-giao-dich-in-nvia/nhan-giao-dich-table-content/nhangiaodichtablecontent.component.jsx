ae2244f9cfa35c1488ba50f35d4c9912a3582a24
Các Lê <cacphuong.le@gmail.com>
2023-12-03 20:59:20 +0700

    Final fix

--- src/components/nhan-giao-dich-in-nvia/nhan-giao-dich-table-content/nhangiaodichtablecontent.component.jsx
+++ src/components/nhan-giao-dich-in-nvia/nhan-giao-dich-table-content/nhangiaodichtablecontent.component.jsx
@@ -1,57 +1,83 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import styles from './nhangiaodichtablecontent.module.css'
+import styles from "./nhangiaodichtablecontent.module.css";
 
-
-/* Component to display each row of table in NhanGiaoDich 
-*   props:  
-*       + data: object of each row to display: {id, time, date, accountName, accountNameTag, transactionID, printerID, docName, pageNumber, 
-*                           confirmation:bool, status:bool, delivery:bool}
-*/
+/* Component to display each row of table in NhanGiaoDich
+ *   props:
+ *       + data: object of each row to display: {id, time, date, accountName, accountNameTag, transactionID, printerID, docName, pageNumber,
+ *                           confirmation:bool, status:bool, delivery:bool}
+ */
 
 const TableRow = (props) => {
-    const data = props.data;
-    
-    return(
-        <tr>
-            <td>
-            <div className={styles.time}>{data.time}</div>
-            <div className={styles.date}>{data.data}</div>
-            </td>
-            <td>
-            <div className={styles.accountName}>{data.accountName}</div>
-            <div className={styles.accountNameTag}>{data.accountNameTag}</div>
-            </td>
-            <td>{data.transactionID}</td>
-            <td>{data.printerID}</td>
-            <td>{data.docName}</td>
-            <td>{data.pageNumber}</td>
-            <td>
-            <input type="radio" name={"confirmation"+data.id}  value="Confirm" defaultChecked={data.confirmation}/>
-            <span >Xác nhận</span>
-            <br />
-            <input type="radio" name={"confirmation"+data.id}  value="UnConfirm" defaultChecked={!data.confirmation}/>
-            <span >Chưa xác nhận</span>
-            </td>
-            <td>
-            <input type="radio" name={"status"+data.id}  value="Printed" defaultChecked={data.status}/>
-            <span >Đã in</span>
-            <br />
-            <input type="radio" name={"status"+data.id}  value="UnPrinted" defaultChecked={!data.status}/>
-            <span >Chưa in</span>
-            </td>
-            <td>
-            <input type="radio" name={"delivery"+data.id}  value="Delivered" defaultChecked={data.delivery}/>
-            <span >Đã in</span>
-            <br />
-            <input type="radio" name={"delivery"+data.id} value="UnDelivered" defaultChecked={!data.delivery}/>
-            <span >Chưa in</span>
-            </td>
-        </tr>
+  const data = props.data;
 
-        
-    );
-
-}
+  return (
+    <tr>
+      <td>
+        <div className={styles.time}>{data.time}</div>
+        <div className={styles.date}>{data.data}</div>
+      </td>
+      <td>
+        <div className={styles.accountName}>{data.accountName}</div>
+        <div className={styles.accountNameTag}>{data.accountNameTag}</div>
+      </td>
+      <td>{data.transactionID}</td>
+      <td>{data.printerID}</td>
+      <td>{data.docName}</td>
+      <td>{data.pageNumber}</td>
+      <td>
+        <input
+          type="radio"
+          name={"confirmation" + data.id}
+          value="Confirm"
+          defaultChecked={data.confirmation}
+        />
+        <span>Xác nhận</span>
+        <br />
+        <input
+          type="radio"
+          name={"confirmation" + data.id}
+          value="UnConfirm"
+          defaultChecked={!data.confirmation}
+        />
+        <span>Chưa xác nhận</span>
+      </td>
+      <td>
+        <input
+          type="radio"
+          name={"status" + data.id}
+          value="Printed"
+          defaultChecked={data.status}
+        />
+        <span>Đã in</span>
+        <br />
+        <input
+          type="radio"
+          name={"status" + data.id}
+          value="UnPrinted"
+          defaultChecked={!data.status}
+        />
+        <span>Chưa in</span>
+      </td>
+      <td>
+        <input
+          type="radio"
+          name={"delivery" + data.id}
+          value="Delivered"
+          defaultChecked={data.delivery}
+        />
+        <span>Đã giao</span>
+        <br />
+        <input
+          type="radio"
+          name={"delivery" + data.id}
+          value="UnDelivered"
+          defaultChecked={!data.delivery}
+        />
+        <span>Chưa giao</span>
+      </td>
+    </tr>
+  );
+};
 
 export default TableRow;
--- src/components/thong-so-in/thongsoin.components.jsx
+++ src/components/thong-so-in/thongsoin.components.jsx
@@ -1,95 +1,102 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import './thongsoin.styles.css'
+import "./thongsoin.styles.css";
 import Popup from "../popup/popup.component";
 import Button from "../button/button.component";
 import XacNhanGiaoDich from "../xac-nhan-giao-dich/xacnhangiaodich.component";
 
 const ThongSoIn = (props) => {
-    // useState var
-    // const [xacNhanGiaoDich, setXacNhanGiaoDich] = useState(false);
-    let printProperties ={};
+  // useState var
+  // const [xacNhanGiaoDich, setXacNhanGiaoDich] = useState(false);
+  let printProperties = {};
 
-    return(
-        <Popup openPopup={props.openPopup}>
-            <div className="container">
-                <div className="popup-title"><h1>Tùy chỉnh thông số in</h1></div>
-                <div className="main-page">
-                    <div className="preview-container">
-                        <div className="preview-title">Xem trước</div>
-                        <div className="preview-page"></div>
-                    </div>
-                    <div className="adjust-container">
-                        <div className="adjust-row">
-                            <label htmlFor="">Số bản:</label>
-                            <input type="number" name="" id="" value={1} />
-                        </div>
-                        <div className="adjust-row size-select">
-                            <label htmlFor="">Kích thước:</label>
-                            <select name="" id="">
-                                <option value="A4">A3</option>
-                                <option value="A3">A3</option>
-                            </select>
-                        </div>
-                        <div className="adjust-row">
-                            <label htmlFor="">In từ trang:  </label>
-                            <input type="number" name="fromPage" id="" />
-                            <span> đến </span>
-                            <input type="number" name="toPage" id="" />
-                        </div>
-                        <div className="adjust-row print-direction">
-                            <label htmlFor="">Hướng in:</label>
-                            <input type="radio" name="direction" id="" value="vertical" checked/>
-                            <span>Dọc</span>
-                            <input type="radio" name="direction" id="" value="horizontal"/>
-                            <span>Ngang</span>
-                        </div>
-                        <div className="adjust-row align-row">
-                            <label htmlFor="">Canh lề (Inch)</label>
-                            <div>
-                                <div>
-                                    <span>Trái:</span>
-                                    <input type="number" name="left" id="" value={1}/>
-                                    <span>Phải:</span>
-                                    <input type="number" name="right" id="" value={1}/>
-                                </div>
-                                <div>
-                                    <span>Trên:</span>
-                                    <input type="number" name="top" id="" value={1}/>
-                                    <span>Dưới:</span>
-                                    <input type="number" name="bottom" id="" value={1}/>
-                                </div>
-                                
-                             
-                            </div>
-
-                        </div>
-                        <div className="adjust-row">
-                            <label htmlFor="">Số trang/1 mặt:</label>
-                            <input type="number" name="" id="" value={1}/>
-                        </div>
-                    </div>
-                </div>
-                <div className="save-option">
-                    <label htmlFor="">Lưu tài liệu trong vòng 7 ngày</label>
-                    <input type="checkbox" name="" id="" />
-                </div>
-                <div className="popup-footer">
-                    <Button className='button-footer' onClick={()=>props.setXacNhanGiaoDich(true)}>Xác nhận</Button>
-                    <Button className='button-footer' onClick={()=>props.openPopup(false)}>Quay lại</Button>
-                </div>
-                
+  return (
+    <Popup openPopup={props.openPopup}>
+      <div className="container">
+        <div className="popup-title">
+          <h1>Tùy chỉnh thông số in</h1>
+        </div>
+        <div className="main-page">
+          <div className="preview-container">
+            <div className="preview-title">Xem trước</div>
+            <div className="preview-page"></div>
+          </div>
+          <div className="adjust-container">
+            <div className="adjust-row">
+              <label htmlFor="">Số bản:</label>
+              <input type="number" name="" id="" defaultValue={1} />
             </div>
-            
-        </Popup>
-
-        
-    );
-
-
-
-
-
-}
+            <div className="adjust-row size-select">
+              <label htmlFor="">Kích thước:</label>
+              <select name="khogiay" defaultValue={"A4"}>
+                <option value="A4">A3</option>
+                <option value="A3">A4</option>
+                <option value="A3">A5</option>
+              </select>
+            </div>
+            <div className="adjust-row">
+              <label htmlFor="">In từ trang: </label>
+              <input type="number" name="fromPage" defaultValue={1} />
+              <span> đến </span>
+              <input type="number" name="toPage" defaultValue={"-"} />
+            </div>
+            <div className="adjust-row print-direction">
+              <label htmlFor="">Hướng in:</label>
+              <input
+                type="radio"
+                name="direction"
+                id=""
+                value="vertical"
+                defaultChecked
+              />
+              <span>Dọc</span>
+              <input type="radio" name="direction" id="" value="horizontal" />
+              <span>Ngang</span>
+            </div>
+            <div className="adjust-row align-row">
+              <label htmlFor="">Canh lề (Inch)</label>
+              <div>
+                <div>
+                  <span>Trái:</span>
+                  <input type="number" name="left" id="" defaultValue={1} />
+                  <span>Phải:</span>
+                  <input type="number" name="right" id="" defaultValue={1} />
+                </div>
+                <div>
+                  <span>Trên:</span>
+                  <input type="number" name="top" id="" defaultValue={1} />
+                  <span>Dưới:</span>
+                  <input type="number" name="bottom" id="" defaultValue={1} />
+                </div>
+              </div>
+            </div>
+            <div className="adjust-row">
+              <label htmlFor="">Số trang/1 mặt:</label>
+              <input type="number" name="" id="" defaultValue={1} />
+            </div>
+          </div>
+        </div>
+        <div className="save-option">
+          <label htmlFor="">Lưu tài liệu trong vòng 7 ngày</label>
+          <input type="checkbox" name="" id="" />
+        </div>
+        <div className="popup-footer">
+          <Button
+            className="button-footer"
+            onClick={() => props.setXacNhanGiaoDich(true)}
+          >
+            Xác nhận
+          </Button>
+          <Button
+            className="button-footer"
+            onClick={() => props.openPopup(false)}
+          >
+            Quay lại
+          </Button>
+        </div>
+      </div>
+    </Popup>
+  );
+};
 
 export default ThongSoIn;
